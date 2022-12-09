0f9c95a2020d1dc39a5d0d892b9d600a81a9a8c5
label3 <jiancazang@163.com>
2022-12-09 22:15:47 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,11 +5,33 @@
 	</dv-border-box-11>
 	<dv-decoration-9 style="width:150px;height:150px;">66%</dv-decoration-9>
 	<dv-decoration-12 style="width:150px;height:150px; "/>
-	<dv-charts :option="option" />
 	<dv-flyline-chart :config="config" style="width:100%;height:100%;" />
+
 	</div>
 </template>
 <script>
-	
+	export default {
+	  centerPoint: [0.48, 0.35],
+	  points: [
+	    [0.52, 0.23],
+	    [0.43, 0.29],
+	    [0.59, 0.35],
+	    [0.53, 0.47],
+	    [0.45, 0.54],
+	    [0.36, 0.38],
+	    [0.62, 0.55],
+	    [0.56, 0.56],
+	    [0.37, 0.66],
+	    [0.55, 0.81],
+	    [0.55, 0.67],
+	    [0.37, 0.29],
+	    [0.20, 0.36],
+	    [0.76, 0.41],
+	    [0.59, 0.18],
+	    [0.68, 0.17],
+	    [0.59, 0.10]
+	  ],
+	  bgImgUrl: '/img/flylineChart/map.jpg'
+	}
 	</script>
 	
